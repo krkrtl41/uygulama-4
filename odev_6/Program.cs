151312fd36b090e58348f4a5adbda4fbc1e6063f
Hp <Hp@DESKTOP-LTJOJD3>
2023-05-27 22:12:47 +0300

    Add project files.

---
+++ odev_6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int sonuc = Faktoriyel(sayi);
+            Console.WriteLine("Sonuç : " + sonuc);
+            Console.ReadKey();
+        }
+
+        static int Faktoriyel(int a)
+        {
+            if(a == 0)
+            {
+                return 1;
+            }
+            else if (a == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return a * Faktoriyel(a - 1);
+            }
+        }
+    }
+}
